e2584266e7c80a265281b19b8455a5afe089ee0d
Fabiola Aponte <fabiiaa89@gmail.com>
2026-05-06 23:33:20 -0500

    Initialize React application with ErrorBoundary

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { ErrorBoundary } from './components/ErrorBoundary';
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </React.StrictMode>
+  );
+}
