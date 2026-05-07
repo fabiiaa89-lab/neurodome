0c55e993de33143a25268ac7eedd7137dbbd15c1
Fabiola Aponte <fabiiaa89@gmail.com>
2026-05-06 23:41:25 -0500

    Add SplashScreen component for initial loading view

---
+++ src/components/SplashScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function SplashScreen() {
+  return (
+    <div style={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center', background: '#000', color: '#fff', fontFamily: 'sans-serif' }}>
+      <h1>NEURO-DOME</h1>
+    </div>
+  );
+}
